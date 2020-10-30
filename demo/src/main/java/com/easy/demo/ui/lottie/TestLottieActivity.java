9101d7f8120d7fd3de251cb06b40d7f2e9962c38
clarence <763287516@qq.com>
2020-10-30 15:40:06 +0800

    添加lottie动画 lottie中包含图片

--- demo/src/main/java/com/easy/demo/ui/lottie/TestLottieActivity.java
+++ demo/src/main/java/com/easy/demo/ui/lottie/TestLottieActivity.java
@@ -1,13 +1,20 @@
 package com.easy.demo.ui.lottie;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
+import com.airbnb.lottie.ImageAssetDelegate;
+import com.airbnb.lottie.LottieImageAsset;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.easy.apt.annotation.ActivityInject;
 import com.easy.demo.R;
 import com.easy.demo.databinding.TestLottieBinding;
 import com.easy.framework.base.BaseActivity;
+import com.easy.utils.SystemUtils;
 
 @ActivityInject
 @Route(path = "/demo/TestLottieActivity", name = "Lottie动画")
@@ -21,10 +28,23 @@ public class TestLottieActivity extends BaseActivity<TestLottiePresenter, TestLo
     @Override
     public void initView() {
         viewBind.lottie.setAnimationFromUrl("https://assets2.lottiefiles.com/packages/lf20_bGW6Oe.json");
+
+//        viewBind.lottie2.setImageAssetDelegate(new ImageAssetDelegate() {
+//            @Nullable
+//            @Override
+//            public Bitmap fetchBitmap(LottieImageAsset asset) {
+//                Bitmap bitmap = BitmapFactory.decodeFile("/sdcard/img_0.jpg");
+//                Log.d("lottie2", bitmap == null ? "null" : "!null");
+//                return bitmap;
+//            }
+//        });
+        viewBind.lottie2.setImageAssetsFolder("images/");
+        viewBind.lottie2.setAnimation("lottie_ceshi.json");
     }
 
     public void testLottieAnimation(View view) {
         viewBind.lottie.playAnimation();
+        viewBind.lottie2.playAnimation();
     }
 
 }
